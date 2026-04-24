522607252489247935aba83c4f8789120bae7047
Alexander <alexandersantamariabayona6@gmail.com>
2026-04-24 17:15:28 -0500

    Desarrollo del service con logica de negocio

---
+++ logica/services/incidenciaService.php
@@ -0,0 +1,33 @@
+<?php
+class incidenciaService {
+
+    public function crearIncidencia($data){
+        if (empty($data["titulo"])){
+            return [
+                "status" => "error",
+                "message" => "El titulo es obligatorio"
+            ];
+        }
+
+        $tiposValidos = ["Bache", "Alumbrado", "Resuidos", "Arboles", "Otro"];
+
+        if (!in_array($data["tipo"], $tiposValidos)) {
+            return [
+                "status" => "error",
+                "message" => "Tipo invalido"
+            ];
+        }
+
+        $data["insert_data"]["estado"] = "Reportado";
+        $data["insert_data"]["prioridad"] = "Media;
+
+        return [
+            "success" => true,
+            "operation" => $data["operation"],
+            "table" => $data["table"],
+            "data_to_save" => $data["insert_data"]
+        ];
+    }
+
+}
+?>
