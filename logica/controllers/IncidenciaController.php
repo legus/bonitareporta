68c6d45e4f9fe174cbcd705f2de98852b47ea7ed
Alexander <alexandersantamariabayona6@gmail.com>
2026-04-24 17:44:45 -0500

    Implementacion de Controller de incidencias

--- logica/controllers/IncidenciaController.php
+++ logica/controllers/IncidenciaController.php
@@ -3,5 +3,5 @@ require_once __DIR__ . "/../services/incidenciaService.php";
 $data = json_decode(file_get_contents("php://input"), true);
 $service = new incidenciaService();
 $response = $service->crearIncidencia($data);
-echo json_encode("$response");
+echo json_encode($response);
 ?>
